60ae2fc4021920aad4f0c55591474454bb2f173d
Nick Genovese <ngenovese@pepperdash.com>
2022-03-09 11:07:29 -0500

    initial solution setup

---
+++ src/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Crestron.SimplSharp;                          				// For Basic SIMPL# Classes
+using Crestron.SimplSharpPro;                       				// For Basic SIMPL#Pro classes
+
+namespace PanoptoCloudEpi
+{
+    public class Class1
+    {
+    }
+}
+
---
+++ src/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("PanoptoCloudEpi")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PanoptoCloudEpi")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
