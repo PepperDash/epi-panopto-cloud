5a5d04f575e661ef96b9ef501d2a27cca5864cac
Nick Genovese <ngenovese@pepperdash.com>
2022-03-11 18:01:04 -0500

    feat: initial plugin development

---
+++ src/PanoptoCloudControllerFactory.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Core.Config;
+
+namespace PanoptoCloudEpi
+{
+    public class PanoptoCloudControllerFactory : EssentialsPluginDeviceFactory<PanoptoCloudController>
+    {
+        public PanoptoCloudControllerFactory()
+        {
+            TypeNames = new List<string> {"panopto", "panoptocloud"};
+            MinimumEssentialsFrameworkVersion = "1.9.7";
+        }
+
+        public override EssentialsDevice BuildDevice(DeviceConfig dc)
+        {
+            return new PanoptoCloudController(dc);
+        }
+    }
+}
